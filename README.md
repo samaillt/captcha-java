390c8f5265014eb9b3e08f805da898561edad92e
Tom Samaille <tomsamaille@gmailcom>
2019-06-02 23:10:28 +0200

    Command line update

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ From the git project root:
 cd ./CAPTCHA
 ```
 ```
-find . -name "*.java"  -print | xargs javac -d classes
-```
-```
 rsync -avz --exclude '*.java' ./sources/ ./classes/
+```
+```
+find . -name "*.java"  -print | xargs javac -d classes
 ```
 ```
 java -cp classes fr.upem.captcha.ui.MainUi
--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-cd ./CAPTCHA && find . -name "*.java"  -print | xargs javac -d classes && rsync -avz --exclude '*.java' ./sources/ ./classes/ && java -cp classes fr.upem.captcha.ui.MainUi
+cd ./CAPTCHA && rsync -avz --exclude '*.java' ./sources/ ./classes/ && find . -name "*.java"  -print | xargs javac -d classes && java -cp classes fr.upem.captcha.ui.MainUi
